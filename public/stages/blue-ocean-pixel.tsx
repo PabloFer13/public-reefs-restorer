4d7c539bf79eaf834aaf206f1d7a9b43642b3741
Emmanuel Yupit <t269690g@techdata.com>
2021-10-23 18:45:56 -0500

    level view update, WIP

--- public/stages/blue-ocean-pixel.tsx
+++ public/stages/blue-ocean-pixel.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="blue-ocean-pixel" tilewidth="16" tileheight="16" tilecount="5160" columns="120">
- <image source="../../../../Downloads/blue-ocean-pixel.png" trans="ff00ff" width="1920" height="700"/>
+ <image source="../../../../Documents/personal docs/universidad/imgs/blue-ocean-pixel.png" trans="ff00ff" width="1920" height="700"/>
 </tileset>
--- src/containers/Level/index.js
+++ src/containers/Level/index.js
@@ -1,58 +1,28 @@
 import { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { Sprite, Stage, Container } from '@inlet/react-pixi';
 import * as PIXI from 'pixi.js';
-import { Sprite, Stage, withApp } from 'react-pixi-fiber';
-import { SimpleLightmapFilter } from '@pixi/filter-simple-lightmap';
-// import { Howl } from 'howler';
-import actions from 'actions';
-import LevelMenu from 'components/Menu/LevelMenu';
-// import WaveBackground from 'components/WaveBackground';
-import pearlImg from 'assets/images/pearl.png';
-import woodImg from 'assets/images/wood.png';
-import rockImg from 'assets/images/rock.png';
-import lightmap from 'assets/images/lightmap.png';
-import reef from 'assets/images/reef.jpeg';
-// import BgSound from 'assets/music/bg-sea-1.mp3';
+// import bgWater from 'assets/images/level/level.png';
+// import bgWater from 'assets/images/level/level-filter1.jpg';
+// import bgWater from 'assets/images/level/level-filter2.jpg';
+// import bgWater from 'assets/images/level/level-filter3.jpg';
+// import bgWater from 'assets/images/level/level-filter4.jpg';
+// import bgWater from 'assets/images/level/level-filter5.jpg';
+// import bgWater from 'assets/images/level/level-filter6.jpg';
+import bgWater from 'assets/images/level/level-filter7.jpg';
+import brainFish from 'assets/images/reefFlora/cerebro.png';
+import pink from 'assets/images/reefFlora/pink.png';
+import purple from 'assets/images/reefFlora/purple.png';
+import green from 'assets/images/reefFlora/green.png';
+import red from 'assets/images/reefFlora/red.png';
+import lightOrange from 'assets/images/reefFlora/light-orange.png';
+import Bubbles from 'components/Bubbles';
 
-const OPTIONS = {
-  backgroundColor: 0x1099bb,
-  style: {
-    transform: 'scale 1.1',
-  },
-};
-
-const lightmapFilter = new SimpleLightmapFilter(
-  new PIXI.Texture.from(lightmap),
-  [50, 0, 0, 0.015]
-);
-
-const ResourceItem = ({ amount, image, alt }) => (
-  <figure className="flex py-1">
-    <img src={image} alt={alt} className="h-6 w-6" />
-    <p className="text-white font-bubble">
-      :<span className="ml-4">{amount}</span>
-    </p>
-  </figure>
-);
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
 
 const Level = () => {
-  // const bgMusic = new Howl({
-  //   src: BgSound,
-  //   autoplay: false,
-  //   loop: true,
-  // });
-
-  // useEffect(() => {
-  //   bgMusic.play();
-  //   return () => {
-  //     bgMusic.stop();
-  //   };
-  // }, []);
-
-  const [filters, setFilters] = useState([lightmapFilter]);
+  const [filters, setFilters] = useState([]);
   const displacementRef = useRef(null);
-  const dispatch = useDispatch();
-  const resources = useSelector(({ level: { resources } }) => resources);
 
   useEffect(() => {
     const animate = () => {
@@ -65,96 +35,85 @@ const Level = () => {
     );
     displacementRef.current.texture.baseTexture.wrapMode =
       PIXI.WRAP_MODES.REPEAT;
-    displacementRef.current.scale.x = 5;
-    displacementRef.current.scale.y = 5;
+    displacementRef.current.scale.x = 10;
+    displacementRef.current.scale.y = 10;
     setFilters(prev => [...prev, displacementFilter]);
     animate();
-    dispatch(actions.level.getUserResources());
-    dispatch(actions.level.getLevelDetails());
-  }, [dispatch]);
+  }, []);
 
-  const removeFilter = () => {
-    setFilters(([x, ...prev]) => [...prev]);
-  };
-
-  const { gold, rocks, pearls, wood } = resources;
-  const resourcesMenu = [
+  const reefs = [
     {
-      amount: pearls,
-      image: pearlImg,
-      alt: 'pearl',
+      x: screenWidth / 2 - 480,
+      y: screenHeight / 2 - 10,
+      scale: 0.7,
+      texture: new PIXI.Texture.from(purple),
     },
     {
-      amount: wood,
-      image: woodImg,
-      alt: 'wood',
+      x: screenWidth / 2 - 320,
+      y: screenHeight / 2 + 50,
+      scale: 0.75,
+      texture: new PIXI.Texture.from(pink),
     },
     {
-      amount: rocks,
-      image: rockImg,
-      alt: 'rock',
+      x: screenWidth / 2 + 610,
+      y: screenHeight / 2 + 40,
+      scale: 0.5,
+      texture: new PIXI.Texture.from(green),
     },
     {
-      amount: gold,
-      image: pearlImg,
-      alt: 'pearl',
+      x: screenWidth / 2 + 450,
+      y: screenHeight / 2 + 90,
+      scale: 0.75,
+      texture: new PIXI.Texture.from(lightOrange),
+    },
+    {
+      x: -60,
+      y: screenHeight / 2 + 50,
+      scale: 0.75,
+      texture: new PIXI.Texture.from(red),
     },
   ];
 
   return (
-    <Stage
-      options={OPTIONS}
-      width={1}
-      height={1}
-      filters={filters}
-      className="h-screen w-screen">
-      <Sprite
-        texture={PIXI.Texture.from(reef, {
-          scaleMode: PIXI.SCALE_MODES.LINEAR,
-        })}
-      />
-
-      <Sprite
-        texture={PIXI.Texture.from(
-          'https://cdna.artstation.com/p/assets/images/images/009/070/412/large/alisha-bannister-sarno-alishabs-clouds.jpg?1516950864'
-        )}
-        ref={displacementRef}
-      />
-      <LevelMenu position="0,0" />
-    </Stage>
+    <>
+      <Stage
+        width={screenWidth}
+        height={screenHeight}
+        options={{ resizeTo: window }}>
+        <Sprite
+          texture={
+            new PIXI.Texture.from(bgWater, {
+              scaleMode: PIXI.SCALE_MODES.LINEAR,
+            })
+          }
+          width={screenWidth}
+          height={screenHeight}
+          filters={filters}
+        />
+        <Sprite
+          width={screenWidth}
+          height={screenHeight}
+          texture={PIXI.Texture.from(
+            'https://cdna.artstation.com/p/assets/images/images/009/070/412/large/alisha-bannister-sarno-alishabs-clouds.jpg?1516950864'
+          )}
+          ref={displacementRef}
+        />
+        <Container filters={filters}>
+          {reefs.map(it => (
+            <Sprite
+              x={it.x}
+              y={it.y}
+              scale={it.scale}
+              texture={it.texture}
+              interactive
+              buttonMode
+            />
+          ))}
+        </Container>
+      </Stage>
+      <Bubbles />
+    </>
   );
 };
 
-export default withApp(Level);
-
-//ignore. could be usefull for menu styling
-
-/* <div className="h-screen bg-gray-800">
-      <div className="">
-        <div className="py-20 px-24">
-          <div className="border-4 p-8 border-yellow-400">
-            <p className="text-center text-white text-6xl font-bubble">
-              Arrecife del Oceano Pacífico
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row">
-          <div className="flex mx-auto my-auto">
-            <div className="grid justify-items-center">
-              {resourcesMenu.map(resourceProps => (
-                <ResourceItem {...resourceProps} />
-              ))}
-              <div className="">
-                <button
-                  className="text-white"
-                  type="button"
-                  onClick={removeFilter}>
-                  RM Filter
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className=""></div>
-        </div>
-      </div>
-    </div> */
+export default Level;
